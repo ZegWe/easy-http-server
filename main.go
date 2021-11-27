0fbebbaff82fb80ee7848c076a5aa784005a9e9a
ZegWe <abdccdba147@gmail.com>
2021-11-28 01:25:33 +0800

    display internal ip address

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
 
@@ -15,13 +16,13 @@ func main() {
 	var port string
 	if len(os.Args) > 1 {
 		port = os.Args[1]
-	}else {
+	} else {
 		port = ":8080"
 	}
 	log.Println(str)
 	fs := http.FileServer(http.Dir(str))
 	f := handlers.CustomLoggingHandler(log.Writer(), fs, func(writer io.Writer, params handlers.LogFormatterParams) {
-		str:= fmt.Sprintf("%v %v %v %v\n",
+		str := fmt.Sprintf("%v %v %v %v\n",
 			params.TimeStamp.Local().Format("2006/01/02 15:04:05"),
 			params.StatusCode,
 			params.Request.Method,
@@ -30,5 +31,22 @@ func main() {
 		writer.Write([]byte(str))
 	})
 	http.Handle("/", f)
+	log.Println("Listening on", getInternalIP(), port)
 	log.Panic(http.ListenAndServe(port, nil))
+}
+
+func getInternalIP() (ips []string) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				ips = append(ips, ipnet.IP.String())
+			}
+		}
+	}
+	return
 }
